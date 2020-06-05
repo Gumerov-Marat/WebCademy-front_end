76fac291babd5b78b93b5b12f8923f684eae6f46
Marat Gumerov <gumerovm74@gmail.com>
2020-06-05 23:23:09 +0300

    step4: watch refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,47 @@
 const gulp = require('gulp'); // Подключаем Gulp
 const browserSync = require('browser-sync').create();
 const watch = require('gulp-watch');
-const sass = require('gulp-sass')
-const sourceMaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 
+// Таск для компиляции SCSS в CSS
+gulp.task('scss', function(callback) {
+	return gulp.src('./app/scss/main.scss')
+		.pipe( sourcemaps.init() )
+		.pipe( sass() )
+		.pipe( autoprefixer({
+			overrideBrowserslist: ['last 4 versions']
+		}) )
+		.pipe( sourcemaps.write() )
+		.pipe( gulp.dest('./app/css/') )
+	callback();
+});
 
-gulp.task('scss', function (callback) {
-  return gulp.src('./app//scss/*.scss')
-    .pipe( sourceMaps.init())
-    .pipe( sass())
-    .pipe(autoprefixer({
-      overrideBrowserslist:['last 4 versions']
-    }))
-    .pipe( sourceMaps.write())
-    .pipe( gulp.dest('./app/css/'));
-  callback();
-})
+// Слежение за HTML и CSS и обновление браузера
+gulp.task('watch', function() {
+	// Слежение за HTML и CSS и обновление браузера
+	watch(['./app/*.html', './app/css/**/*.css'], gulp.parallel( browserSync.reload ));
 
-gulp.task('watch', function () {
-  watch(['./app/*.html', './app/css/**/*.css'], gulp.parallel(browserSync.reload));
+	// Слежение за SCSS и компиляция в CSS - обычный способ
+	// watch('./app/scss/**/*.scss', gulp.parallel('scss'));
+
+	// Запуск слежения и компиляции SCSS с задержкой, для жесктих дисков HDD
+	watch('./app/scss/**/*.scss', function(){
+		setTimeout( gulp.parallel('scss'), 1000 )
+	})
+
 });
 
 // Задача для старта сервера из папки app
-gulp.task('server', function () {
-  browserSync.init({
-    server: {
-      baseDir: "./app/"
-    }
-  })
+gulp.task('server', function() {
+	browserSync.init({
+		server: {
+			baseDir: "./app/"
+		}
+	})
 });
 
-gulp.task('default', gulp.parallel('server', 'watch'));
+// Дефолтный таск (задача по умолчанию)
+// Запускаем одновременно задачи server и watch
+gulp.task('default', gulp.parallel('server', 'watch', 'scss'));
