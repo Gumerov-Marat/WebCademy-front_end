a38e35faa48a9681277cfce8408b5ab8919037ec
Marat Gumerov <gumerovm74@gmail.com>
2020-06-05 22:46:52 +0300

    step1: add scss npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 const gulp = require('gulp'); // Подключаем Gulp
 const browserSync = require('browser-sync').create();
 const watch = require('gulp-watch');
+const sass = require('gulp-sass')
+
+
+gulp.task('scss', function (callback) {
+  return gulp.src('./app//scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./app/css/'));
+  callback();
+})
 
 gulp.task('watch', function () {
   watch(['./app/*.html', './app/css/**/*.css'], gulp.parallel(browserSync.reload));
