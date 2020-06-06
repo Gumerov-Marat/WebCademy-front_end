2aabd5b54b7e81ef821312dfb02152c871126863
Marat Gumerov <gumerovm74@gmail.com>
2020-06-06 14:24:10 +0300

    step1: refactoring gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,10 @@ const sourcemaps = require('gulp-sourcemaps');
 const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
 const pug = require('gulp-pug');
+const del = require('del');
 
 // Таск для сборки Gulp файлов
-gulp.task('pug', function() {
+gulp.task('pug', function(callback) {
 	return gulp.src('./src/pug/pages/**/*.pug')
 		.pipe( plumber({
 			errorHandler: notify.onError(function(err){
@@ -24,7 +25,8 @@ gulp.task('pug', function() {
 			pretty: true
 		}) )
 		.pipe( gulp.dest('./build/') )
-		.pipe(browserSync.stream())
+		.pipe( browserSync.stream() )
+	callback();
 });
 
 // Таск для компиляции SCSS в CSS
@@ -46,22 +48,41 @@ gulp.task('scss', function(callback) {
 		}) )
 		.pipe( sourcemaps.write() )
 		.pipe( gulp.dest('./build/css/') )
-		.pipe( browserSync.stream())
+		.pipe( browserSync.stream() )
+	callback();
+});
+
+// Копирование Изображений
+gulp.task('copy:img', function(callback) {
+	return gulp.src('./src/img/**/*.*')
+	  .pipe(gulp.dest('./build/img/'))
+	callback();
+});
+
+// Копирование Скриптов
+gulp.task('copy:js', function(callback) {
+	return gulp.src('./src/js/**/*.*')
+	  .pipe(gulp.dest('./build/js/'))
 	callback();
 });
 
 // Слежение за HTML и CSS и обновление браузера
 gulp.task('watch', function() {
-	// Слежение за HTML и CSS и обновление браузера
-	watch(['./build/*.html'], gulp.parallel( browserSync.reload ));
+
+	// Следим за картинками и скриптами и обновляем браузер
+	watch( ['./build/js/**/*.*', './build/img/**/*.*'], gulp.parallel(browserSync.reload) );
 
 	// Запуск слежения и компиляции SCSS с задержкой
 	watch('./src/scss/**/*.scss', function(){
-		setTimeout( gulp.parallel('scss'), 1000 )
+		setTimeout( gulp.parallel('scss'), 500 )
 	})
 
 	// Слежение за PUG и сборка
 	watch('./src/pug/**/*.pug', gulp.parallel('pug'))
+
+	// Следим за картинками и скриптами, и копируем их в build
+	watch('./src/img/**/*.*', gulp.parallel('copy:img'))
+	watch('./src/js/**/*.*', gulp.parallel('copy:js'))
 
 });
 
@@ -74,6 +95,17 @@ gulp.task('server', function() {
 	})
 });
 
+gulp.task('clean:build', function() {
+	return del('./build')
+});
+
 // Дефолтный таск (задача по умолчанию)
 // Запускаем одновременно задачи server и watch
-gulp.task('default', gulp.parallel('server', 'watch', 'scss', 'pug'));
+gulp.task(
+		'default', 
+		gulp.series( 
+			gulp.parallel('clean:build'),
+			gulp.parallel('scss', 'pug', 'copy:img', 'copy:js'), 
+			gulp.parallel('server', 'watch'), 
+			)
+	);
